9392b22fe75eae82deb0a85289684bfddc7fa068
MayconFCruz <dev.mayconcruz@gmail.com>
2021-04-05 14:36:56 -0300

    Deletei algumas linhas de comando!

--- Aula/src/AulaUm/Exercicios_1.java
+++ Aula/src/AulaUm/Exercicios_1.java
@@ -12,18 +12,7 @@ public class Exercicios_1 {
 		  
 		   System.out.println("A primeira letra do seu nome é "+x);
 		
-		 String x1;
-		 int y1;
-		 double z1;
-		 
-		 x1 = sc.next();
-		 y1 = sc.nextInt();
-		 z1 = sc.nextDouble();
-		 
-		 System.out.println(x1);
-		 System.out.println(y1);
-		 System.out.println(z1);
-		 sc.close();
+		
 		
 		
 	}
