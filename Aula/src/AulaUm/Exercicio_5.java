43074b3b3420cb1f1e6bea2a83d8e522569a377f
Computec_TI <Computec_TI@DESKTOP-B69LENB>
2021-04-15 16:46:34 -0300

    Exercício_5.java finalizado

--- Aula/src/AulaUm/Exercicio_5.java
+++ Aula/src/AulaUm/Exercicio_5.java
@@ -1,8 +1,13 @@
 package AulaUm;
 
+import java.util.*;
+
 public class Exercicio_5 {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner( System.in );
+		
 		
 	/*	Fazer um programa que leia o número de um 
 		funcionário, seu número de horas trabalhadas,
@@ -12,9 +17,32 @@ public class Exercicio_5 {
 		do funcionário, com duas casas decimais.  
 		
 Entrada: 25 100 5.50***********Saída: Númber = 25 Salary = U$ 550.00
-Entrada: 1 100 20.50 **********Saída Númber = 1 Salary = U$ 4100.00		
+Entrada: 1 200 20.50 **********Saída Númber = 1 Salary = U$ 4100.00		
 Entrada: 6 145 15.55 **********Saída Númber = 6 Salary = U$ 2254.75				
 	                                                                 */
+		 
+		    
+		    int numeroFuncionario=0;
+		    int horasTrabalhadas=0;
+			double x=0;
+			double totalSalario=0;
+			
+			
+			System.out.println("Digite o número do funcionario ");
+			numeroFuncionario = input.nextInt();
+			System.out.println("Digite o total de horas trabalhadas: ");
+			horasTrabalhadas = input.nextInt();
+			System.out.println("Digite o salario do funcionário: ");
+			x = input.nextDouble();
+			
+			totalSalario = (horasTrabalhadas*x);
+			
+			System.out.printf("Funcionario número: "+numeroFuncionario+" valor do salário a receber:%.2f%n ",totalSalario);
+	  
+		
+			
+		
+		
 		
 		
 	}
