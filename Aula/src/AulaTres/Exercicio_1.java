754c7b94dbc844d1c1843fd18933c8d7b24bea1e
Maycon_Computec_TI <Computec_TI@DESKTOP-B69LENB>
2021-05-04 16:32:27 -0300

    Concluído o exercício_1 do pacote aula tres!

---
+++ Aula/src/AulaTres/Exercicio_1.java
@@ -0,0 +1,36 @@
+package AulaTres;
+import java.util.Scanner;
+
+public class Exercicio_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int senha=0;
+		
+		
+		
+		
+		while(senha!=2002) {
+			
+			
+				
+			System.out.println("Senha invalida! ");
+			senha = sc.nextInt();
+		}
+		
+		
+		System.out.println("Acesso permitido! ");
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
